453350013c1e5e7eff7d7c31d3ca5cdd8e463ff6
Vladimir <freshcode.yt@gmail.com>
2021-02-03 18:59:04 +0200

    Add promise fetch example

--- index.js
+++ index.js
@@ -1,35 +1,11 @@
 'use strict';
 
-/* 
-  Создать объект. У объекта - (строка, число, undefined, null, [])
-  и 2 метода.
-  Сериализовать его. JSON.stringify
-  Десериализовать JSON.parse
+const p = fetch('./data.json');
 
-*/
+const jsonPromise = p.then((response) => {
+  return response.json();
+});
 
-const user = {
-  firstName: 'Vlad',
-  lastName: 'Testovich',
-  age: 45,
-  getName() {
-    return this.firstName;
-  },
-  isSelected: null,
-  isArgee: undefined,
-  isMale: true,
-  [Symbol('test')]: {},
-  phones: ['648237462387', '234234324', '2342342342'],
-  friend: {
-    firstName: '1',
-    lastName: '2',
-  },
-};
-
-const serializedObject = JSON.stringify(user); // сериализация
-
-console.log(serializedObject);
-
-const recoveredUser = JSON.parse(serializedObject);
-
-console.log(recoveredUser);
+jsonPromise.then((data) => {
+  console.table(data);
+});
